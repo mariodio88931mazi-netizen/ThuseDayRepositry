2240fafe0e1721c1049737c256fb2542e62ff7d0
uemuro <mariodio88931mazi@gmail.com>
2026-08-25 16:26:34 +0900

    Add files via upload

---
+++ 20260825/20260825_Tack2.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Calculator
+{
+private:
+	double num1;
+	double num2;
+
+	//
+	Calculator(double n1, double n2)
+	{
+		num1 = n1;
+		num2 = n2;
+	}
+
+	//‰ÁZ
+	double add()
+	{
+		return num1 + num2;
+	}
+
+	//Œ¸Z
+	double subtract()
+	{
+		return num1 - num2;
+	}
+
+	//æZ
+	double multiply()
+	{
+		return num1 * num2;
+	}
+
+	//œZ
+	double divide()
+	{
+		return num1 / num2;
+	}
+};
+
+int main()
+{
+	int number1, number2;
+	int answer;
+	cout << "”š‚ğ‚QŒÂ“ü—ÍF" << endl;
+	cin >> number1,number2;
+	cout << "‚Ç‚ÌŒvZ‚ğs‚¢‚Ü‚·‚©H" <<  <<<<
+
+
+}
